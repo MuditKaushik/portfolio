cd9a6daa7b85c4b670c838d2b0bdf84e9d2ac409
Mudit_Kaushik <muditm18@gmail.com>
2024-03-09 17:33:03 +0530

    remove-docs-folder

--- docs/static/js/846.0d5d1e89.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[846],{846:(o,e,r)=>{r.r(e),r.d(e,{PortfolioPage:()=>PortfolioPage});var t=r(950),l=r(414);class PortfolioPage extends t.Component{render(){return(0,l.jsx)(t.Fragment,{children:(0,l.jsx)("h1",{children:"Portfolio"})})}}}}]);
